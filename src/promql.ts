131ed99be1b5b637b157e798b87ec63129c37658
Augustin Husson <husson.augustin@gmail.com>
2020-03-08 00:46:28 +0100

    fixing linter error

--- src/promql.ts
+++ src/promql.ts
@@ -1,7 +1,7 @@
 import { StringStream } from "codemirror";
 
 export interface PromQLState {
-  tokenize: { (stream: StringStream, state: PromQLState): null | string }[]
+  tokenize: { (stream: StringStream, state: PromQLState): null | string }[];
 }
 
 // PromQL Aggregation Operators
@@ -107,7 +107,121 @@ const floatSuffix = /[fFlL]?/;
 // Merging all the keywords in one list
 const keywords = aggregations.concat(functions).concat(aggregationsOverTime).concat(vectorMatching).concat(offsetModifier);
 
-export function tokenBase(stream: StringStream, state: PromQLState) {
+function readBlockComment(stream: StringStream): string {
+  stream.skipToEnd();
+  return 'comment'
+}
+
+function readString(stream: StringStream): void {
+  let next = null;
+  let previous = null;
+  while ((next = stream.next()) != null) {
+    if ((next === '\'' || next === '"') && previous !== '\\') {
+      return
+    }
+    previous = next
+  }
+}
+
+function readNumber(currentChar: string, stream: StringStream): void {
+  const integerRegex = /\d*[\d']*\d*/;
+  const floatRegex = /\d*\.?\d+([eE][\-+]?\d+)?/;
+  const hexRegex = /[xX][0-9a-fA-F']*[0-9a-fA-F]/;
+  const octalRegex = /[0-7']*[0-7]/;
+  const binaryRegex = /[bB][0-1']*[0-1]/;
+  // 24h, 5m are often encountered in prometheus
+  if (stream.match(/\d*[smhdwy]/)) {
+    return;
+  }
+  if (stream.match(new RegExp(floatRegex.source + "(" + floatSuffix.source + ")"))) {
+    return;
+  }
+
+  if (stream.match(new RegExp(integerRegex.source + "(" + integerSuffix.source + ")"))) {
+    return;
+  }
+
+  if (currentChar === '0') {
+    if (stream.match(new RegExp(hexRegex.source + "(" + integerSuffix.source + ")"))) { // hex
+      return;
+    }
+    if (stream.match(new RegExp(octalRegex.source + "(" + integerSuffix.source + ")"))) { // octal
+      return;
+    }
+    stream.match(new RegExp(binaryRegex.source + "(" + integerSuffix.source + ")")) // binary
+  }
+}
+
+function readVectorMatchingVariable(stream: StringStream, state: PromQLState): null | string {
+  // first eat all space
+  stream.eatSpace();
+  const nextChar = stream.peek();
+  if (nextChar === ',') {
+    // it's the label separator, do nothing excepting consuming the next char
+    stream.next();
+    return null
+  }
+  if (nextChar === ')') {
+    // that's the end of the vectorMatching declaration. Stop using the current function
+    // don't consume the nextChar because it will be used by the method readVectorMatchingVariable to also remove it in the stack
+    state.tokenize.pop()
+  }
+  // now move the cursor to the next word and declare it's a variable
+  stream.eatWhile(/[\w$_-]/);
+  return "variable"
+}
+
+function readVectorMatching(stream: StringStream, state: PromQLState): null {
+  // first eat all space
+  stream.eatSpace();
+  // check if we have an open bracket, if we don't, just leave because it's or a wrong syntax or we are at the end of the matchingVector declaration
+  const ch = stream.next();
+  if (ch !== '(') {
+    state.tokenize.pop();
+    return null
+  }
+  // now use the function relative to the vectorMatching variable
+  state.tokenize.push(readVectorMatchingVariable);
+  return null
+}
+
+function readLabel(stream: StringStream, state: PromQLState): null | string {
+  // first eat all space
+  stream.eatSpace();
+  const ch = stream.next();
+  if (!ch) {
+    return null
+  }
+  if (ch === '}') {
+    // that's the end of the label declaration. Stop using the current function and go back to tokenBase function
+    state.tokenize.pop();
+    return null
+  }
+  if (ch === ',') {
+    // it's the label separator, do nothing
+    return null
+  }
+  // check if it's the beginning of a string
+  if (ch === '\'' || ch === '"') {
+    readString(stream);
+    return 'string'
+  }
+  // check if it's a short operator
+  if (isShortOperator.test(ch)) {
+    stream.eatWhile(isShortOperator);
+    return "operator";
+  }
+  // check if it's the beginning of a number
+  if (/\d/.test(ch)) {
+    readNumber(ch, stream);
+    return 'number'
+  }
+  // now move the cursor to the next word and check if it's a known word
+  stream.eatWhile(/[\w$_-]/);
+  return 'variable'
+}
+
+export function tokenBase(stream: StringStream, state: PromQLState): null | string {
   if (stream.peek() === '#') {
     return readBlockComment(stream)
   }
@@ -172,117 +286,4 @@ export function tokenBase(stream: StringStream, state: PromQLState) {
   }
 
   return null
-}
-
-function readBlockComment(stream: StringStream): string {
-  stream.skipToEnd();
-  return 'comment'
-}
-
-function readString(stream: StringStream) {
-  let next = null;
-  let previous = null;
-  while ((next = stream.next()) != null) {
-    if ((next === '\'' || next === '"') && previous !== '\\') {
-      return
-    }
-  }
-}
-
-function readNumber(currentChar: string, stream: StringStream) {
-  const integerRegex = /\d*[\d']*\d*/;
-  const floatRegex = /\d*\.?\d+([eE][\-+]?\d+)?/;
-  const hexRegex = /[xX][0-9a-fA-F']*[0-9a-fA-F]/;
-  const octalRegex = /[0-7']*[0-7]/;
-  const binaryRegex = /[bB][0-1']*[0-1]/;
-  // 24h, 5m are often encountered in prometheus
-  if (stream.match(/\d*[smhdwy]/)) {
-    return;
-  }
-  if (stream.match(new RegExp(floatRegex.source + "(" + floatSuffix.source + ")"))) {
-    return;
-  }
-
-  if (stream.match(new RegExp(integerRegex.source + "(" + integerSuffix.source + ")"))) {
-    return;
-  }
-
-  if (currentChar === '0') {
-    if (stream.match(new RegExp(hexRegex.source + "(" + integerSuffix.source + ")"))) { // hex
-      return;
-    }
-    if (stream.match(new RegExp(octalRegex.source + "(" + integerSuffix.source + ")"))) { // octal
-      return;
-    }
-    stream.match(new RegExp(binaryRegex.source + "(" + integerSuffix.source + ")")) // binary
-  }
-}
-
-function readVectorMatching(stream: StringStream, state: PromQLState): null {
-  // first eat all space
-  stream.eatSpace();
-  // check if we have an open bracket, if we don't, just leave because it's or a wrong syntax or we are at the end of the matchingVector declaration
-  let ch = stream.next();
-  if (ch !== '(') {
-    state.tokenize.pop();
-    return null
-  }
-  // now use the function relative to the vectorMatching variable
-  state.tokenize.push(readVectorMatchingVariable);
-  return null
-}
-
-function readVectorMatchingVariable(stream: StringStream, state: PromQLState) {
-  // first eat all space
-  stream.eatSpace();
-  const nextChar = stream.peek();
-  if (nextChar === ',') {
-    // it's the label separator, do nothing excepting consuming the next char
-    stream.next();
-    return null
-  }
-  if (nextChar === ')') {
-    // that's the end of the vectorMatching declaration. Stop using the current function
-    // don't consume the nextChar because it will be used by the method readVectorMatchingVariable to also remove it in the stack
-    state.tokenize.pop()
-  }
-  // now move the cursor to the next word and declare it's a variable
-  stream.eatWhile(/[\w$_-]/);
-  return "variable"
-}
-
-function readLabel(stream: StringStream, state: PromQLState) {
-  // first eat all space
-  stream.eatSpace();
-  const ch = stream.next();
-  if (!ch) {
-    return null
-  }
-  if (ch === '}') {
-    // that's the end of the label declaration. Stop using the current function and go back to tokenBase function
-    state.tokenize.pop();
-    return null
-  }
-  if (ch === ',') {
-    // it's the label separator, do nothing
-    return null
-  }
-  // check if it's the beginning of a string
-  if (ch === '\'' || ch === '"') {
-    readString(stream);
-    return 'string'
-  }
-  // check if it's a short operator
-  if (isShortOperator.test(ch)) {
-    stream.eatWhile(isShortOperator);
-    return "operator";
-  }
-  // check if it's the beginning of a number
-  if (/\d/.test(ch)) {
-    readNumber(ch, stream);
-    return 'number'
-  }
-  // now move the cursor to the next word and check if it's a known word
-  stream.eatWhile(/[\w$_-]/);
-  return 'variable'
 }
