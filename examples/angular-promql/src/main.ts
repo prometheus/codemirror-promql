9e940b47e3ad2a18a46887e9038cdc292eda498d
Augustin Husson <husson.augustin@gmail.com>
2020-04-13 16:30:08 +0200

    updating angular example using the last package name

--- examples/angular-promql/src/main.ts
+++ examples/angular-promql/src/main.ts
@@ -4,7 +4,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
-import 'codemirror-mode-promql';
+import 'codemirror-promql';
 
 if (environment.production) {
   enableProdMode();
