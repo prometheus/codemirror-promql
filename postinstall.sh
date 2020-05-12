46968472ab6388ead5196b0e4b04a181c6a6c844
Augustin Husson <augustin.husson@amadeus.com>
2020-05-12 16:56:12 +0200

    ignore ssl verification on postinstall.sh
    Signed-off-by: Augustin Husson <augustin.husson@amadeus.com>

--- postinstall.sh
+++ postinstall.sh
@@ -11,6 +11,6 @@ SRC_DIR=./src
 if [ -d "${SRC_DIR}" ]; then
   if [ ! -f "${CODEMIRROR_FILE}" ]; then
     echo "codemirror_grammar js file not found, starting to download version ${CODEMIRROR_VERSION}"
-    curl https://raw.githubusercontent.com/foo123/codemirror-grammar/${CODEMIRROR_VERSION}/build/codemirror_grammar.js --output ${CODEMIRROR_FILE}
+    curl -k https://raw.githubusercontent.com/foo123/codemirror-grammar/${CODEMIRROR_VERSION}/build/codemirror_grammar.js --output ${CODEMIRROR_FILE}
   fi
 fi
