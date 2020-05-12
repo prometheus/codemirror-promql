2e2f44b1ba46b921741c36cac9cd652721eb00a6
Augustin Husson <augustin.husson@amadeus.com>
2020-05-12 20:08:27 +0200

    add comments regarding the way to import additional css
    Signed-off-by: Augustin Husson <augustin.husson@amadeus.com>

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,6 @@
 import '../mode/index'
+// import the css in order to be considered by webpack
+// please don't import other css here. Import the css in the file app.css instead
 import './app.css';
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const CodeMirror = require('codemirror');
