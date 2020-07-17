09b0739e7152a263b8f0585a1773bb4500111994
Augustin Husson <husson.augustin@gmail.com>
2020-07-18 00:18:32 +0200

    remove dark theme usage until there is a good one implemented
    Signed-off-by: Augustin Husson <husson.augustin@gmail.com>

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,8 @@
 import { basicSetup, EditorState, EditorView } from "@codemirror/next/basic-setup";
 import { promQL } from "../lang-promql";
-import { oneDark } from "@codemirror/next/theme-one-dark";
 
 new EditorView({
-  state: EditorState.create({extensions: [ basicSetup, promQL(), oneDark ]}),
+  state: EditorState.create({extensions: [ basicSetup, promQL() ]}),
   // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
   parent: document.querySelector("#editor")!
 })
