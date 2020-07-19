8f0faff280a716393b0df2c3a02b2564ddc6d165
Augustin Husson <husson.augustin@gmail.com>
2020-07-19 17:10:28 +0200

    export promQLCompletion
    Signed-off-by: Augustin Husson <husson.augustin@gmail.com>

--- src/lang-promql/index.ts
+++ src/lang-promql/index.ts
@@ -20,4 +20,4 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
-export { promQLSyntax, promQL } from "./promql"
+export { promQLSyntax, promQLCompletion, promQL } from "./promql"
