4632060fcb4f714a4ffb51c5c7d63bb15cbf4d5e
dvkndn <dvkndn@gmail.com>
2020-10-02 14:04:29 +0700

    Use better (simpler) completion help
    Signed-off-by: dvkndn <dvkndn@gmail.com>

--- src/lang-promql/complete/promql.terms.ts
+++ src/lang-promql/complete/promql.terms.ts
@@ -22,203 +22,191 @@ export const binOpModifierTerms = [{ label: 'on' }, { label: 'ignoring' }, { lab
 export const functionIdentifierTerms: AutoCompleteNode[] = [
   {
     label: 'abs',
-    info: 'Returns the input vector with all sample values converted to their absolute value.',
+    info: 'Return absolute values of input series',
   },
   {
     label: 'absent',
-    info:
-      'Returns an empty vector if the vector passed to it has any elements and a 1-element vector with the value 1 if the vector passed to it has no elements. This is useful for alerting on when no time series exist for a given metric name and label combination.',
+    info: 'Determine whether input vector is empty',
   },
   {
     label: 'absent_over_time',
+    info: 'Determine whether input range vector is empty',
   },
   {
     label: 'avg_over_time',
-    info: 'The average value of all points in the specified interval.',
+    info: 'Average series values over time',
   },
   {
     label: 'ceil',
-    info: 'Rounds the sample values of all elements in `v` up to the nearest integer.',
+    info: 'Round up values of input series to nearest integer',
   },
   {
     label: 'changes',
-    info:
-      'For each input time series, `changes(v range-vector)` returns the number of times its value has changed within the provided time range as an instant vector.',
+    info: 'Return number of value changes in input series over time',
   },
   {
     label: 'clamp_max',
-    info: 'Clamps the sample values of all elements in `v` to have an upper limit of `max`.',
+    info: 'Limit the value of input series to a maximum',
   },
   {
     label: 'clamp_min',
-    info: 'Clamps the sample values of all elements in `v` to have a lower limit of `min`.',
+    info: 'Limit the value of input series to a minimum',
   },
   {
     label: 'count_over_time',
-    info: 'The count of all values in the specified interval.',
+    info: 'Count the number of values for each input series',
   },
   {
     label: 'days_in_month',
-    info: 'Returns number of days in the month for each of the given times in UTC. Returned values are from 28 to 31.',
+    info: 'Return the number of days in current month for provided timestamps',
   },
   {
     label: 'day_of_month',
-    info: 'Returns the day of the month for each of the given times in UTC. Returned values are from 1 to 31.',
+    info: 'Return the day of the month for provided timestamps',
   },
   {
     label: 'day_of_week',
-    info: 'Returns the day of the week for each of the given times in UTC. Returned values are from 0 to 6, where 0 means Sunday etc.',
+    info: 'Return the day of the week for provided timestamps',
   },
   {
     label: 'delta',
-    info:
-      'Calculates the difference between the first and last value of each time series element in a range vector `v`, returning an instant vector with the given deltas and equivalent labels. The delta is extrapolated to cover the full time range as specified in the range vector selector, so that it is possible to get a non-integer result even if the sample values are all integers.',
+    info: 'Calculate the difference between beginning and end of a range vector (for gauges)',
   },
   {
     label: 'deriv',
-    info: 'Calculates the per-second derivative of the time series in a range vector `v`, using simple linear regression.',
+    info: 'Calculate the per-second derivative over series in a range vector (for gauges)',
   },
   {
     label: 'exp',
-    info: 'Calculates the exponential function for all elements in `v`.\nSpecial cases are:\n* `Exp(+Inf) = +Inf` \n* `Exp(NaN) = NaN`',
+    info: 'Calculate exponential function for input vector values',
   },
   {
     label: 'floor',
-    info: 'Rounds the sample values of all elements in `v` down to the nearest integer.',
+    info: 'Round down values of input series to nearest integer',
   },
   {
     label: 'histogram_quantile',
-    info:
-      'Calculates the φ-quantile (0 ≤ φ ≤ 1) from the buckets `b` of a histogram. The samples in `b` are the counts of observations in each bucket. Each sample must have a label `le` where the label value denotes the inclusive upper bound of the bucket. (Samples without such a label are silently ignored.) The histogram metric type automatically provides time series with the `_bucket` suffix and the appropriate labels.',
+    info: 'Calculate quantiles from histogram buckets',
   },
   {
     label: 'holt_winters',
-    info:
-      'Produces a smoothed value for time series based on the range in `v`. The lower the smoothing factor `sf`, the more importance is given to old data. The higher the trend factor `tf`, the more trends in the data is considered. Both `sf` and `tf` must be between 0 and 1.',
+    info: 'Calculate smoothed value of input series',
   },
   {
     label: 'hour',
-    info: 'Returns the hour of the day for each of the given times in UTC. Returned values are from 0 to 23.',
+    info: 'Return the hour of the day for provided timestamps',
   },
   {
     label: 'idelta',
-    info:
-      'Calculates the difference between the last two samples in the range vector `v`, returning an instant vector with the given deltas and equivalent labels.',
+    info: 'Calculate the difference between the last two samples of a range vector (for counters)',
   },
   {
     label: 'increase',
-    info:
-      'Calculates the increase in the time series in the range vector. Breaks in monotonicity (such as counter resets due to target restarts) are automatically adjusted for. The increase is extrapolated to cover the full time range as specified in the range vector selector, so that it is possible to get a non-integer result even if a counter increases only by integer increments.',
+    info: 'Calculate the increase in value over a range of time (for counters)',
   },
   {
     label: 'irate',
-    info:
-      'Calculates the per-second instant rate of increase of the time series in the range vector. This is based on the last two data points. Breaks in monotonicity (such as counter resets due to target restarts) are automatically adjusted for.',
+    info: 'Calculate the per-second increase over the last two samples of a range vector (for counters)',
   },
   {
     label: 'label_replace',
-    info:
-      "For each timeseries in `v`, `label_replace(v instant-vector, dst_label string, replacement string, src_label string, regex string)`  matches the regular expression `regex` against the label `src_label`.  If it matches, then the timeseries is returned with the label `dst_label` replaced by the expansion of `replacement`. `$1` is replaced with the first matching subgroup, `$2` with the second etc. If the regular expression doesn't match then the timeseries is returned unchanged.",
+    info: 'Set or replace label values',
   },
   {
     label: 'label_join',
+    info: 'Join together label values into new label',
   },
   {
     label: 'ln',
-    info:
-      'calculates the natural logarithm for all elements in `v`.\nSpecial cases are:\n * `ln(+Inf) = +Inf`\n * `ln(0) = -Inf`\n * `ln(x < 0) = NaN`\n * `ln(NaN) = NaN`',
+    info: 'Calculate natural logarithm of input series',
   },
   {
     label: 'log10',
-    info: 'Calculates the decimal logarithm for all elements in `v`. The special cases are equivalent to those in `ln`.',
+    info: 'Calulcate base-10 logarithm of input series',
   },
   {
     label: 'log2',
-    info: 'Calculates the binary logarithm for all elements in `v`. The special cases are equivalent to those in `ln`.',
+    info: 'Calculate base-2 logarithm of input series',
   },
   {
     label: 'max_over_time',
-    info: 'The maximum value of all points in the specified interval.',
+    info: 'Return the maximum value over time for input series',
   },
   {
     label: 'min_over_time',
-    info: 'The minimum value of all points in the specified interval.',
+    info: 'Return the minimum value over time for input series',
   },
   {
     label: 'minute',
-    info: 'Returns the minute of the hour for each of the given times in UTC. Returned values are from 0 to 59.',
+    info: 'Return the minute of the hour for provided timestamps',
   },
   {
     label: 'month',
-    info: 'Returns the month of the year for each of the given times in UTC. Returned values are from 1 to 12, where 1 means January etc.',
+    info: 'Return the month for provided timestamps',
   },
   {
     label: 'predict_linear',
-    info: 'Predicts the value of time series `t` seconds from now, based on the range vector `v`, using simple linear regression.',
+    info: 'Predict the value of a gauge into the future',
   },
   {
     label: 'quantile_over_time',
-    info: 'The φ-quantile (0 ≤ φ ≤ 1) of the values in the specified interval.',
+    info: 'Calculate value quantiles over time for input series',
   },
   {
     label: 'rate',
-    info:
-      "Calculates the per-second average rate of increase of the time series in the range vector. Breaks in monotonicity (such as counter resets due to target restarts) are automatically adjusted for. Also, the calculation extrapolates to the ends of the time range, allowing for missed scrapes or imperfect alignment of scrape cycles with the range's time period.",
+    info: 'Calculate per-second increase over a range vector (for counters)',
   },
   {
     label: 'resets',
-    info:
-      'For each input time series, `resets(v range-vector)` returns the number of counter resets within the provided time range as an instant vector. Any decrease in the value between two consecutive samples is interpreted as a counter reset.',
+    info: 'Return number of value decreases (resets) in input series of time',
   },
   {
     label: 'round',
-    info:
-      'Rounds the sample values of all elements in `v` to the nearest integer. Ties are resolved by rounding up. The optional `to_nearest` argument allows specifying the nearest multiple to which the sample values should be rounded. This multiple may also be a fraction.',
+    info: 'Round values of input series to nearest integer',
   },
   {
     label: 'scalar',
-    info:
-      'Given a single-element input vector, `scalar(v instant-vector)` returns the sample value of that single element as a scalar. If the input vector does not have exactly one element, `scalar` will return `NaN`.',
+    info: 'Convert single-element series vector into scalar value',
   },
   {
     label: 'sort',
-    info: 'Returns vector elements sorted by their sample values, in ascending order.',
+    info: 'Sort input series ascendingly by value',
   },
   {
     label: 'sort_desc',
-    info: 'Returns vector elements sorted by their sample values, in descending order.',
+    info: 'Sort input series descendingly by value',
   },
   {
     label: 'sqrt',
-    info: 'Calculates the square root of all elements in `v`.',
+    info: 'Return the square root for input series',
   },
   {
     label: 'stddev_over_time',
-    info: 'The population standard deviation of the values in the specified interval.',
+    info: 'Calculate the standard deviation within input series over time',
   },
   {
     label: 'stdvar_over_time',
-    info: 'The population standard variance of the values in the specified interval.',
+    info: 'Calculate the standard variation within input series over time',
   },
   {
     label: 'sum_over_time',
-    info: 'The sum of all values in the specified interval.',
+    info: 'Calculate the sum over the values of input series over time',
   },
   {
     label: 'time',
-    info:
-      'Returns the number of seconds since January 1, 1970 UTC. Note that this does not actually return the current time, but the time at which the expression is to be evaluated.',
+    info: 'Return the Unix timestamp at the current evaluation time',
   },
   {
     label: 'timestamp',
+    info: 'Return the Unix timestamp for the samples in the input vector',
   },
   {
     label: 'vector',
-    info: 'Returns the scalar `s` as a vector with no labels.',
+    info: 'Convert a scalar value into a single-element series vector',
   },
   {
     label: 'year',
-    info: 'Returns the year for each of the given times in UTC.',
+    info: 'Return the year for provided timestamps',
   },
 ];
 functionIdentifierTerms.forEach((term) => {
